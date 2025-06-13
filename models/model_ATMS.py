893176a7428e91b6c052903277abf97c280423dd
Lukas Heinrich <heilu021@student.otago.ac.nz>
2025-06-14 00:04:56 +1200

    change to native TransformerEncoderLayer and TransformerEncoder
    Formerly implemented TransformerEncoderLayer and TransformerEncoder seemed to be mostly doing the same as the native parts. Most changes were not actively used and hencefore removed or where implementable via options as done.

--- models/model_ATMS.py
+++ models/model_ATMS.py
@@ -7,149 +7,10 @@ from torch import Tensor
 import torch.nn.functional as F
 from torch import distributed as dist
 from einops.layers.torch import Rearrange
-from math import sqrt
 import numpy as np
 
-
-
-# -------------- from subject_layers/Transformer_EncDec.py
-class EncoderLayer(nn.Module):
-    def __init__(self, attention, d_model, d_ff=None, dropout=0.1, activation="relu"):
-        super(EncoderLayer, self).__init__()
-        d_ff = d_ff or 4 * d_model
-        self.attention = attention
-        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
-        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-        self.dropout = nn.Dropout(dropout)
-        self.activation = F.relu if activation == "relu" else F.gelu
-
-    def forward(self, x, attn_mask=None, tau=None, delta=None):
-        new_x, attn = self.attention(
-            x, x, x,
-            attn_mask=attn_mask,
-            tau=tau, delta=delta
-        )
-        x = x + self.dropout(new_x)
-
-        y = x = self.norm1(x)
-        y = self.dropout(self.activation(self.conv1(y.transpose(-1, 1))))
-        y = self.dropout(self.conv2(y).transpose(-1, 1))
-
-        return self.norm2(x + y), attn
-
-
-class Encoder(nn.Module):
-    def __init__(self, attn_layers, conv_layers=None, norm_layer=None):
-        super(Encoder, self).__init__()
-        self.attn_layers = nn.ModuleList(attn_layers)
-        self.conv_layers = nn.ModuleList(conv_layers) if conv_layers is not None else None
-        self.norm = norm_layer
-
-    def forward(self, x, attn_mask=None, tau=None, delta=None):
-        # x [B, L, D]
-        attns = []
-        if self.conv_layers is not None:
-            for i, (attn_layer, conv_layer) in enumerate(zip(self.attn_layers, self.conv_layers)):
-                delta = delta if i == 0 else None
-                x, attn = attn_layer(x, attn_mask=attn_mask, tau=tau, delta=delta)
-                x = conv_layer(x)
-                attns.append(attn)
-            x, attn = self.attn_layers[-1](x, tau=tau, delta=None)
-            attns.append(attn)
-        else:
-            for attn_layer in self.attn_layers:
-                x, attn = attn_layer(x, attn_mask=attn_mask, tau=tau, delta=delta)
-                attns.append(attn)
-
-        if self.norm is not None:
-            x = self.norm(x)
-
-        return x, attns
-    
-# -------------- from subject_layers/SelfAttention_Family.py
-
-
-class AttentionLayer(nn.Module):
-    def __init__(self, attention, d_model, n_heads, d_keys=None,
-                 d_values=None):
-        super(AttentionLayer, self).__init__()
-
-        d_keys = d_keys or (d_model // n_heads)
-        d_values = d_values or (d_model // n_heads)
-
-        self.inner_attention = attention
-        self.query_projection = nn.Linear(d_model, d_keys * n_heads)
-        self.key_projection = nn.Linear(d_model, d_keys * n_heads)
-        self.value_projection = nn.Linear(d_model, d_values * n_heads)
-        self.out_projection = nn.Linear(d_values * n_heads, d_model)
-        self.n_heads = n_heads
-
-    def forward(self, queries, keys, values, attn_mask, tau=None, delta=None):
-        B, L, _ = queries.shape
-        _, S, _ = keys.shape
-        H = self.n_heads
-
-        queries = self.query_projection(queries).view(B, L, H, -1)
-        keys = self.key_projection(keys).view(B, S, H, -1)
-        values = self.value_projection(values).view(B, S, H, -1)
-
-        out, attn = self.inner_attention(
-            queries,
-            keys,
-            values,
-            attn_mask,
-            tau=tau,
-            delta=delta
-        )
-        out = out.view(B, L, -1)
-
-        return self.out_projection(out), attn
-
-class FullAttention(nn.Module):
-    def __init__(self, mask_flag=True, factor=5, scale=None, attention_dropout=0.1, output_attention=False):
-        super(FullAttention, self).__init__()
-        self.scale = scale
-        self.mask_flag = mask_flag
-        self.output_attention = output_attention
-        self.dropout = nn.Dropout(attention_dropout)
-
-    def forward(self, queries, keys, values, attn_mask, tau=None, delta=None):
-        B, L, H, E = queries.shape
-        _, S, _, D = values.shape
-        scale = self.scale or 1. / sqrt(E)
-
-        scores = torch.einsum("blhe,bshe->bhls", queries, keys)
-
-        if self.mask_flag:
-            if attn_mask is None:
-                attn_mask = TriangularCausalMask(B, L, device=queries.device)
-
-            scores.masked_fill_(attn_mask.mask, -np.inf)
-
-        A = self.dropout(torch.softmax(scale * scores, dim=-1))
-        V = torch.einsum("bhls,bshd->blhd", A, values)
-
-        if self.output_attention:
-            return V.contiguous(), A
-        else:
-            return V.contiguous(), None
-        
-# -------------- from utils/masking.py
-
-class TriangularCausalMask():
-    def __init__(self, B, L, device="cpu"):
-        mask_shape = [B, 1, L, L]
-        with torch.no_grad():
-            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
-
-    @property
-    def mask(self):
-        return self._mask
     
 # -------------- from subject_layers/Embed.py
-
 
 class PositionalEmbedding(nn.Module):
     def __init__(self, d_model, max_len=5000):
@@ -191,7 +52,6 @@ class FixedEmbedding(nn.Module):
     def forward(self, x):
         return self.emb(x).detach()
 
-
 class TemporalEmbedding(nn.Module):
     def __init__(self, d_model, embed_type='fixed', freq='h'):
         super(TemporalEmbedding, self).__init__()
@@ -232,7 +92,6 @@ class TimeFeatureEmbedding(nn.Module):
 
     def forward(self, x):
         return self.embed(x)
-
 
 class SubjectEmbedding(nn.Module):
     def __init__(self, num_subjects, d_model):
@@ -377,34 +236,41 @@ class ClipLoss(nn.Module):
 
 class iTransformer(nn.Module):
     def __init__(self, configs, num_subjects=10):
-        super(iTransformer, self).__init__()
+        super().__init__()
         self.seq_len = configs.seq_len
-        self.output_attention = configs.output_attention
-        # Embedding
-        self.enc_embedding = DataEmbedding(configs.seq_len, configs.d_model, configs.embed, configs.freq, configs.dropout, num_subjects=num_subjects)
-        # Encoder
-        self.encoder = Encoder(
-            [
-                EncoderLayer(
-                    AttentionLayer(
-                        FullAttention(False, configs.factor, attention_dropout=configs.dropout, output_attention=configs.output_attention),
-                        configs.d_model, configs.n_heads
-                    ),
-                    configs.d_model,
-                    configs.d_ff,
-                    dropout=configs.dropout,
-                    activation=configs.activation
-                ) for l in range(configs.e_layers)
-            ],
-            norm_layer=torch.nn.LayerNorm(configs.d_model)
+        self.d_model = configs.d_model
+        # Embedding remains the same
+        self.enc_embedding = DataEmbedding(
+            c_in=configs.enc_in,
+            d_model=configs.d_model,
+            embed_type=configs.embed,
+            freq=configs.freq,
+            dropout=configs.dropout,
+            num_subjects=num_subjects
+        )
+        # Native TransformerEncoder
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=configs.d_model,
+            nhead=configs.n_heads,
+            dim_feedforward=configs.d_ff,
+            dropout=configs.dropout,
+            activation=configs.activation,
+            batch_first=True,
+            norm_first=False  # or True for pre-norm behavior
+        )
+        self.encoder = nn.TransformerEncoder(
+            encoder_layer,
+            num_layers=configs.e_layers,
+            norm=nn.LayerNorm(configs.d_model)
         )
 
-    def forward(self, x_enc, x_mark_enc, subject_ids=None):
-        # Embedding
+    def forward(self, x_enc: Tensor, x_mark_enc: Tensor, subject_ids=None) -> Tensor:
+        # x_enc: (B, seq_len, c_in); x_mark_enc: (B, seq_len, time_feats)
         enc_out = self.enc_embedding(x_enc, x_mark_enc, subject_ids)
-        enc_out, attns = self.encoder(enc_out, attn_mask=None)
-        enc_out = enc_out[:, :63, :]      
-        # print("enc_out", enc_out.shape)
+        # mask or src_key_padding_mask can be passed here if needed
+        enc_out = self.encoder(enc_out)  # returns (B, seq_len+1, d_model)
+        # slice off subject token if present and reduce to num_channels
+        enc_out = enc_out[:, :63, :] #configs.enc_in
         return enc_out
 
 
