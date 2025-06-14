344f250af18f0e332884f84273c548bd6343dd49
Lukas Heinrich <heilu021@student.otago.ac.nz>
2025-06-14 13:56:12 +1200

    removed unused Embeddings Code

--- models/model_ATMS.py
+++ models/model_ATMS.py
@@ -1,6 +1,10 @@
 # model taken from: https://github.com/ncclab-sustech/EEG_Image_decode (Only Change: removal of Horovod package)
+# Changes done:
+# Removed never used code: Subject depandant layers, Masking in Transformer, different Embeddings
+# ---> ALTOUGH Postional Embedding is mentioned in paper but was never actually used...
+# .... TODO: Mention in paper/thesis
+# Changed custom implemented transformer to use native Transformer
 
-import math
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -11,87 +15,6 @@ import numpy as np
 
     
 # -------------- from subject_layers/Embed.py
-
-class PositionalEmbedding(nn.Module):
-    def __init__(self, d_model, max_len=5000):
-        super(PositionalEmbedding, self).__init__()
-        # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model).float()
-        pe.require_grad = False
-
-        position = torch.arange(0, max_len).float().unsqueeze(1)
-        div_term = (torch.arange(0, d_model, 2).float()
-                    * -(math.log(10000.0) / d_model)).exp()
-
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        return self.pe[:, :x.size(1)]
-    
-class FixedEmbedding(nn.Module):
-    def __init__(self, c_in, d_model):
-        super(FixedEmbedding, self).__init__()
-
-        w = torch.zeros(c_in, d_model).float()
-        w.require_grad = False
-
-        position = torch.arange(0, c_in).float().unsqueeze(1)
-        div_term = (torch.arange(0, d_model, 2).float()
-                    * -(math.log(10000.0) / d_model)).exp()
-
-        w[:, 0::2] = torch.sin(position * div_term)
-        w[:, 1::2] = torch.cos(position * div_term)
-
-        self.emb = nn.Embedding(c_in, d_model)
-        self.emb.weight = nn.Parameter(w, requires_grad=False)
-
-    def forward(self, x):
-        return self.emb(x).detach()
-
-class TemporalEmbedding(nn.Module):
-    def __init__(self, d_model, embed_type='fixed', freq='h'):
-        super(TemporalEmbedding, self).__init__()
-
-        minute_size = 4
-        hour_size = 24
-        weekday_size = 7
-        day_size = 32
-        month_size = 13
-
-        Embed = FixedEmbedding if embed_type == 'fixed' else nn.Embedding
-        if freq == 't':
-            self.minute_embed = Embed(minute_size, d_model)
-        self.hour_embed = Embed(hour_size, d_model)
-        self.weekday_embed = Embed(weekday_size, d_model)
-        self.day_embed = Embed(day_size, d_model)
-        self.month_embed = Embed(month_size, d_model)
-
-    def forward(self, x):
-        x = x.long()
-        minute_x = self.minute_embed(x[:, :, 4]) if hasattr(
-            self, 'minute_embed') else 0.
-        hour_x = self.hour_embed(x[:, :, 3])
-        weekday_x = self.weekday_embed(x[:, :, 2])
-        day_x = self.day_embed(x[:, :, 1])
-        month_x = self.month_embed(x[:, :, 0])
-
-        return hour_x + weekday_x + day_x + month_x + minute_x
-    
-class TimeFeatureEmbedding(nn.Module):
-    def __init__(self, d_model, embed_type='timeF', freq='h'):
-        super(TimeFeatureEmbedding, self).__init__()
-
-        freq_map = {'h': 4, 't': 5, 's': 6,
-                    'm': 1, 'a': 1, 'w': 2, 'd': 3, 'b': 3}
-        d_inp = freq_map[freq]
-        self.embed = nn.Linear(d_inp, d_model, bias=False)
-
-    def forward(self, x):
-        return self.embed(x)
 
 class SubjectEmbedding(nn.Module):
     def __init__(self, num_subjects, d_model):
@@ -108,22 +31,17 @@ class SubjectEmbedding(nn.Module):
             return self.subject_embedding(subject_ids).unsqueeze(1)
     
 class DataEmbedding(nn.Module):
-    def __init__(self, c_in, d_model, embed_type='fixed', freq='h', dropout=0.1, num_subjects=None):
+    def __init__(self, c_in, d_model, dropout=0.1, num_subjects=None):
         super(DataEmbedding, self).__init__()
 
         self.value_embedding = nn.Linear(c_in, d_model)  # 如果没有指定subjects，则使用单一的value embedding
 
-        self.position_embedding = PositionalEmbedding(d_model=d_model)
-        self.temporal_embedding = TemporalEmbedding(d_model=d_model, embed_type=embed_type, freq=freq) if embed_type != 'timeF' else TimeFeatureEmbedding(d_model=d_model, embed_type=embed_type, freq=freq)
         self.dropout = nn.Dropout(p=dropout)
         self.subject_embedding = SubjectEmbedding(num_subjects, d_model) if num_subjects is not None else None
         self.mask_token = nn.Parameter(torch.randn(1, d_model))  # Mask token embedding
         
-    def forward(self, x, x_mark, subject_ids=None, mask=None):
+    def forward(self, x, subject_ids=None, mask=None):
         x = self.value_embedding(x)
-
-        if x_mark is not None:
-            x = x + self.temporal_embedding(x_mark) + self.position_embedding(x)
 
         if mask is not None:
             x = x * (~mask.bool()) + self.mask_token * mask.float()
@@ -243,8 +161,6 @@ class iTransformer(nn.Module):
         self.enc_embedding = DataEmbedding(
             c_in=configs.enc_in,
             d_model=configs.d_model,
-            embed_type=configs.embed,
-            freq=configs.freq,
             dropout=configs.dropout,
             num_subjects=num_subjects
         )
@@ -264,9 +180,9 @@ class iTransformer(nn.Module):
             norm=nn.LayerNorm(configs.d_model)
         )
 
-    def forward(self, x_enc: Tensor, x_mark_enc: Tensor, subject_ids=None) -> Tensor:
-        # x_enc: (B, seq_len, c_in); x_mark_enc: (B, seq_len, time_feats)
-        enc_out = self.enc_embedding(x_enc, x_mark_enc, subject_ids)
+    def forward(self, x_enc: Tensor, subject_ids=None) -> Tensor:
+        # x_enc: (B, seq_len, c_in)
+        enc_out = self.enc_embedding(x_enc, subject_ids)
         # mask or src_key_padding_mask can be passed here if needed
         enc_out = self.encoder(enc_out)  # returns (B, seq_len+1, d_model)
         # slice off subject token if present and reduce to num_channels
@@ -352,8 +268,6 @@ class Config:
         self.seq_len = 250                 # Sequence length
         self.output_attention = False      # Whether to output attention weights
         self.d_model = 250                 # Model dimension
-        self.embed = 'timeF'               # Time encoding method
-        self.freq = 'h'                    # Time frequency
         self.dropout = 0.25                # Dropout rate
         self.factor = 1                    # Attention scaling factor
         self.n_heads = 4                   # Number of attention heads
@@ -374,7 +288,7 @@ class ATMS(nn.Module):
         self.loss_func = ClipLoss()       
          
     def forward(self, x, subject_ids):
-        x = self.encoder(x, None, subject_ids)
+        x = self.encoder(x, subject_ids)
         eeg_embedding = self.enc_eeg(x)
         
         out = self.proj_eeg(eeg_embedding)
